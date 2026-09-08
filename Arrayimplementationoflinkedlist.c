ee7d82b0625f25092e8d00d999da37f099b3e704
vanshikaaa07 <uvanshika456@gmail.com>
2026-09-08 14:26:49 +0530

    Update Arrayimplementationoflinkedlist.c

--- Arrayimplementationoflinkedlist.c
+++ Arrayimplementationoflinkedlist.c
@@ -14,21 +14,21 @@ int main() {
     printf("Enter number of nodes: ");
     scanf("%d", &n);
 
-    // Input data
+    
     for (i = 0; i < n; i++) {
         printf("Enter data for node %d: ", i + 1);
         scanf("%d", &list[i].data);
 
         if (i == n - 1)
-            list[i].next = -1;   // Last node
+            list[i].next = -1;  
         else
             list[i].next = i + 1;
     }
 
-    // Traversal
+  
     printf("\nLinked List: ");
 
-    i = 0;  // Start from first node
+    i = 0;  
 
     while (i != -1) {
         printf("%d -> ", list[i].data);
